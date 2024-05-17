978ffc12e416982c08d15dd5fd0af4c4ffa237e3
Juan Pablo <totomarion@gmail.com>
2024-05-17 18:48:48 -0300

    SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'widget/scaffold.dart';
+import 'widget/safe_area.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: MyScaffold(),
+      home: MySafeArea(),
     );
   }
 }
--- lib/widget/safe_area.dart
+++ lib/widget/safe_area.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+class MySafeArea extends StatelessWidget {
+  const MySafeArea({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.grey,
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+                width: double.infinity,
+                child: const SafeArea(
+                  bottom: false,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("test@test.com"),
+                      Text("test@test.com"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: Container(
+              color: Colors.red,
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
